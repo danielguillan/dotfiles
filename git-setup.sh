e79624d46edb9f0cdbdf85926f8cdf5978a9d9f3
Daniel Guillan <dguillan@newrelic.com>
2021-07-02 17:09:20 +0200

    test

--- git-setup.sh
+++ git-setup.sh
@@ -1,6 +1,9 @@
 ## Github
 ssh-keygen -t rsa -C "dguillan@newrelic.com"
 
+# Git config
+brew install diff-so-fancy
+
 # Git global config
 git config --global user.name "Daniel Guillan"
 git config --global user.email "dguillan@newrelic.com"
@@ -8,3 +11,4 @@ git config --global github.user danielguillan
 git config --global core.editor "code -w"
 git config --global color.ui true
 git config --global core.excludesfile ~/.gitignore_global
+git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
